7ccbaa5f5c773c1c83b08808eaf7bc8ca52e4978
claraaa-devy <claramaris42@gmail.com>
2025-04-14 21:45:24 +0800

    Add files via upload

---
+++ jvscript.js
@@ -0,0 +1,21 @@
+let lastScrollTop = 0;
+const header = document.querySelector("header");
+const timeText = document.querySelector(".time")
+
+window.addEventListener("scroll", () => {
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  
+  if (scrollTop > lastScrollTop) {
+    header.classList.add("hidden");
+  } else {
+    header.classList.remove("hidden");
+  }
+  
+  lastScrollTop = scrollTop;
+});
+
+
+const yearText = document.getElementById("year");
+const year = new Date().getFullYear();
+
+yearText.textContent = year;
